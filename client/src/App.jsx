414f0b8f5cfb58fb3f4687379b27a7e342003bd9
Haripradap <140403832+Haripradap@users.noreply.github.com>
2024-04-22 12:02:40 +0530

    create pages and routes completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
 
 function App() {
- 
-
   return (
     <>
-     
+     <Routes>
+        <Route path='/' element={<Home/> }/>
+        <Route path='/about' element={ <About/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/project' element={<Project/>}/>
+     </Routes>
     </>
   )
 }
---
+++ client/src/pages/Project.jsx
@@ -0,0 +1,9 @@
+
+
+const Project = () => {
+  return (
+    <div>Project</div>
+  )
+}
+
+export default Project
