1461684fda58f20c53cb4b4e13f8ee7dc1637634
Haripradap <140403832+Haripradap@users.noreply.github.com>
2024-04-24 19:40:17 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,20 @@
 
 import User from "../models/user.model.js";
 import bcryptjs from  'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) => {
+export const signup = async (req,res, next) => {
     const {username ,email , password} = req.body;
 
-    if(!username || 
+    if (
+       !username || 
        !email ||
        !password ||
         username === '' ||
         email === '' ||
         password === ''
-        )
-        {
-        return res.status(400).json({ messaage: "all fields are required!"});
+        ) {
+            return next(errorHandler(400, 'All fields are required'));
         }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +28,6 @@ export const signup = async (req,res) => {
         await newUser.save();
         res.json('signup successful');
     } catch (error) {
-        res.status(500).json({message: error.message}); 
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -16,13 +16,20 @@ mongoose.connect(process.env.MONGO)
 });
 const app = express();
 
-app.use(express.json()); //allow json file to work on backend
+app.use(express.json()); //allow json file to work on backend 
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
 
-
-
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err , req, res , next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode
+    });
+});
